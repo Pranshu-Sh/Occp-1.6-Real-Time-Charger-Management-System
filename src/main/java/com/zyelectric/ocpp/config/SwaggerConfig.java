3b41c79641744c2207ee8b2c8114bcfc68de005d
Pranshu Sharma <106588854+Pranshu-Sh@users.noreply.github.com>
2025-05-04 22:45:20 +0530

    Add files via upload

---
+++ src/main/java/com/zyelectric/ocpp/config/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package com.zyelectric.ocpp.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.*;
+import io.swagger.v3.oas.models.security.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("My API")
+                        .version("1.0")
+                        .description("API documentation with JWT"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("Authorization")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
+}
